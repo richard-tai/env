8a4221eb67f07b8970f428cf8a3be730a3500992
Richard Tai <richard_tai@trendmicro.com.cn>
2018-03-27 14:32:35 +0800

    generate ssh key

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+user=vagrant
+home_dir=/home/${user}/
 
 add-apt-repository ppa:jonathonf/vim -y
 add-apt-repository ppa:openjdk-r/ppa -y
 apt update
 
 apt-get install -y vim
+
 apt-get install -y openjdk-8-jdk
+cat "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/bin" >> ${home_dir}/.bashrc
+
+apt-get install -y ssh
 
 apt-get install -y git
 apt-get install -y curl
@@ -24,14 +30,16 @@ apt-get install -y tmux
 apt-get install -y ctags
 apt-get install -y cscope
 
+apt-get install -y expect
+apt-get install -y rsync
+
 # 安装docker
 if [ ! -f /usr/bin/docker ]; then
 	wget -qO- https://get.docker.com/ | sh
-	usermod -aG docker vagrant
+	usermod -aG docker ${user}
 fi
 
 # setup env
-home_dir=/home/vagrant/
 cd ${home_dir}
 if [ ! -d  .vim/bundle/ ]; then
 	mkdir -p .vim/bundle/
@@ -57,5 +65,10 @@ if [ ! -f  ${home_dir}/.tmux.conf ]; then
 	cp .tmux.conf ${home_dir}
 fi
 
-chown -R vagrant:vagrant ${home_dir}/.vim
+chown -R ${user}:${user} ${home_dir}/.vim
+
+su - ${user}
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >>  ~/.ssh/authorized_keys
+
 

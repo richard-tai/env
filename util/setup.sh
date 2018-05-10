33a1becc494d5784359899cdc1f7bfc64ed864f2
richard_tai <cage0423@gmail.com>
2018-05-10 09:19:21 +0200

    install java

--- util/setup.sh
+++ util/setup.sh
@@ -45,6 +45,12 @@ install_package() {
 		echo ${p}
 		apt-get install -y ${p};
 	done
+
+	# install java
+	#sudo add-apt-repository ppa:webupd8team/java -y
+	#sudo apt-get update
+	#sudo apt-get install oracle-java8-installer
+	#sudo apt-get install oracle-java8-set-default
 }
 
 

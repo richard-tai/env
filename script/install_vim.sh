41f4f72637857b4dc547dc03c1ce507353785259
richard-tai <cage0423@gmail.com>
2020-02-10 23:11:47 +0800

    install vim from source

---
+++ script/install_vim.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+source utils.sh
+
+start_time=$(date +"%s")
+
+if [[ ! -d ~/github/ ]]; then
+    mkdir -p ~/github
+fi
+
+vim_name=vim-8.2.0236
+wget_file_with_cache https://github.com/vim/vim/archive/${vim_name}.zip ~/github/vim.zip
+
+cd ~/github/
+unzip -q vim.zip -d vim
+if [[ $? != 0 ]]; then
+    echo "bad zip"
+    exit 1
+fi
+
+cd vim/${vim_name}
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=$(python3-config --configdir) \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 \
+            --enable-cscope \
+            --prefix=/usr/local
+make -j4 VIMRUNTIMEDIR=/usr/local/share/vim/vim82
+echo ${sudo_passwd} | sudo -S make install
+
+echo "used $(($(date +"%s")-${start_time})) seconds in [$0]."
---
+++ script/utils.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+## global variable
+sudo_passwd=Cat#0987
+
+
+## config
+which git
+if [[ $? -eq 0 ]]; then
+    git config --global http.postBuffer 1048576000
+fi
+
+
+function get_file_extenstion()
+{
+    echo $(echo $1 | sed 's/^.*\.//')
+}
+
+function get_file_name()
+{
+    echo $(echo $1 | sed 's/\.[^.]*$//')
+}
+
+function check_zip() {
+    if [ -d tmp ]; then
+        rm -rf tmp
+    fi
+    unzip -q $1 -d tmp
+    ret=$?
+    if [ -d tmp ]; then
+        rm -rf tmp
+    fi
+    return ${ret}
+}
+
+function check_tar_gz() {
+    if [ -d tmp ]; then
+        rm -rf tmp
+    fi
+    mkdir tmp
+    tar -xzf  $1 -C tmp
+    ret=$?
+    if [ -d tmp ]; then
+        rm -rf tmp
+    fi
+    return ${ret}
+}
+
+function wget_file_with_cache() {
+    echo "wget [$1] to [$2]."
+    extension=$(get_file_extenstion $1)
+    echo "file extension: [${extension}]."
+
+    if [ -f $2 ]; then
+        if [ "$extension" == "zip" ]; then
+            check_zip $2
+        elif [ "$extension" == "gz" ]; then
+            check_tar_gz $2
+        fi
+
+        if [ $? -eq 0 ]; then
+            echo "use exist [$2]."
+            return 0
+        fi
+    fi
+
+    wget -O $2 -c -t 0 $1
+
+    if [ "$extension" == "zip" ]; then
+        check_zip $2
+    elif [ "$extension" == "gz" ]; then
+        check_tar_gz $2
+    fi
+
+    if [ $? -ne 0 ]; then
+        echo "bad $2, please check manually."
+        return 2
+    fi
+}
+
+function unzip_to_dir() {
+    zip_path=$1
+    raw_name=$2
+    dest_name=$3
+    echo "unzip [${zip_path}] to [${raw_name}], rename to [${dest_name}]"
+
+    if [ ! -d ${dest_name} ]; then
+        mkdir -p ${dest_name}
+    fi
+
+    for one_dir in tmp ${dest_name}
+    do
+        if [ -d "${one_dir}" ]; then
+            rm -rf ${one_dir}
+        fi
+    done
+
+    unzip -q ${zip_path} -d tmp
+    mv tmp/${raw_name} ${dest_name}
+
+    if [ -d tmp ]; then
+        rm -rf tmp
+    fi
+}
+
+## unit test
+#echo $(get_file_extenstion aa.bb.cc.tar)
+#echo $(get_file_name aa.bb.cc.tar)

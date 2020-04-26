9fb878c164e6843d360862ec7aaacc12424613e9
richard-tai <cage0423@gmail.com>
2020-04-26 18:23:48 +0800

    Update setup_vim_8.sh

--- script/setup_vim_8.sh
+++ script/setup_vim_8.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source utils.sh
+
 # https://tpaschalis.github.io/vim-go-setup/
 
 install_go() {
@@ -7,16 +9,38 @@ install_go() {
     sudo rm -rf /usr/local/go /usr/bin/go
     wget -c -t 0 https://dl.google.com/go/go1.14.linux-amd64.tar.gz
     sudo tar -C /usr/local/ -xzf go1.14.linux-amd64.tar.gz
-    # echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-    
+    go_path=$(which go)
+    if [ $go_path != "/usr/local/go/bin/go" ]; then
+        echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+        source ~/.bashrc
+    fi  
 }
 
 install_deps() {
     echo "install_deps..."
     install_go
-    sudo dnf install ctags cscope -y
-    # for ycm
-    sudo dnf install cmake gcc-c++ make python3-devel -y
+    os=$(get_os_name)
+    version=$(get_os_version)
+    echo [$os] [$version] ----------------------------
+
+    if [ $os == "Ubuntu" ]; then
+        if [ $version == "20.04" ]; then
+            sudo apt-get install -y ctags cscope
+            # for ycm
+            sudo apt-get install -y cmake g++ make python3-dev
+        else
+            sudo apt-get install -y ctags cscope
+            # for ycm
+            sudo apt-get install -y cmake gcc-c++ make python3-devel
+        fi
+    fi  
+
+    if [ $os == "CentOS" ]; then
+        sudo dnf install ctags cscope -y
+        # for ycm
+        sudo dnf install cmake gcc-c++ make python3-devel -y
+    fi  
+
 }
 
 install_vim_plugin() {
@@ -55,7 +79,7 @@ copy_vim_config() {
 }
 
 setup_vim() {
-    #install_deps
+    install_deps
     copy_vim_config
     install_vim_plugin
 }
